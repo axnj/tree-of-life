fddd248d8a1232925b97dc8766dfdaf6799f7639
axtck-nj <alexander.tack@nowjobs.com>
2021-10-22 00:16:31 +0200

    Add vscode settings

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,19 @@
 import express, { Application } from "express";
 const app: Application = express();
 
+app.get("/", (req, res) => {
+    const resl = doSomething("qsdf");
+    res.json({
+        message: "Base route"
+    });
+});
+
+const doSomething = (test: string) => {
+    return "somethign" + test;
+};
+
+doSomething("qfqs");
 
 app.listen(3000, () => {
     console.log("Listening");
-})
-
-
-
+});
